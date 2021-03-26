c96d227ef1a307ccca9409aadc827bad8ff4885d
AhmedBnDhm <ahmed.bendahmen@esprit.tn>
2021-03-26 22:04:46 +0100

    integration v2

---
+++ migrations/Version20210325150908.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210325150908 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE freelancer ADD date_creation VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE societe ADD date_creation VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE freelancer DROP date_creation');
+        $this->addSql('ALTER TABLE societe DROP date_creation');
+    }
+}
